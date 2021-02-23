5231d1f82986778a6bb85589701e9b8f65e5cd34
Ionut Sauciuc <ionut.sauciuc2003@gmail.com>
2021-02-23 14:12:30 +0200

    secret key

--- the_diverse_api/the_diverse_api/settings.py
+++ the_diverse_api/the_diverse_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .SECRETKEY import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')p*xk0va#2-g84f)1o+cd_ftgo4-x9yu#t_9e$=)0jab7-#qdq'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
