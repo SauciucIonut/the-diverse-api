85e6774f5be7533b25c8220ca3edb88d18597ef3
Ionut Sauciuc <ionut.sauciuc2003@gmail.com>
2021-03-01 13:36:15 +0200

    now using dynamic linking in templates

--- the_diverse_api/api/urls.py
+++ the_diverse_api/api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import CatImagesLink, DogImagesLink, home, endpoints, documentation, login, register
 
 urlpatterns = [
-    path('api/animals/cats/', CatImagesLink.as_view()),
-    path('api/animals/dogs/', DogImagesLink.as_view()),
-    path('', home),
-    path('endpoints/', endpoints),
-    path('documentation/', documentation),
-    path('login/', login),
-    path('register/', register),
+    path('api/animals/cats/', CatImagesLink.as_view(), name="api-animals-cats"),
+    path('api/animals/dogs/', DogImagesLink.as_view(), name="api-animals-dogs"),
+    path('', home, name="index"),
+    path('endpoints/', endpoints, name="endpoints"),
+    path('documentation/', documentation, name="documentation"),
+    path('login/', login, name="login"),
+    path('register/', register, name="register"),
 ]
