ee46e0bf8a7acefd42e7f69eeab0f3825a650dff
Ionut Sauciuc <ionut.sauciuc2003@gmail.com>
2021-03-02 08:22:47 +0200

    refactored views

--- the_diverse_api/api/views.py
+++ the_diverse_api/api/views.py
@@ -12,18 +12,37 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# API ENDPOINTS ----------------------------------------------------------------------------
 
-# FRONT-END -------------------------------------------------------------------------------
+class CatImagesLink(generics.ListCreateAPIView):
+    serializer_class = CatImageSerializer
+    def get(self, request):
+        return Response(CatImageSerializer(random.choice(list(CatImage.objects.all())), many=False).data)
 
-def home(request):
-    return render(request, 'home.html')
+    def post(self, request):
+        # don't accept any post requests for now
+        return Response({'error':'not accepting POSTs for now'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = CatImageSerializer(data=request.data)
 
-def endpoints(request):
-    return render(request, 'endpoints.html')
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def documentation(request):
-    return render(request, 'documentation.html')
+class DogImagesLink(generics.ListCreateAPIView):
+    serializer_class = CatImageSerializer
+    def get(self, request):
+        return Response(DogImageSerializer(random.choice(list(DogImage.objects.all())), many=False).data)
+
+    def post(self, request):
+        # don't accept any post requests for now
+        return Response({'error':'not accepting POSTs for now'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = DogImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # USER LOGIN LOGOUT REGISTER ---------------------------------------------------------------
 
@@ -58,34 +77,13 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
-# API ENDPOINTS ----------------------------------------------------------------------------
+# FRONT-END -------------------------------------------------------------------------------
 
-class CatImagesLink(generics.ListCreateAPIView):
-    serializer_class = CatImageSerializer
-    def get(self, request):
-        return Response(CatImageSerializer(random.choice(list(CatImage.objects.all())), many=False).data)
+def home(request):
+    return render(request, 'home.html')
 
-    def post(self, request):
-        # don't accept any post requests for now
-        return Response({'error':'not accepting POSTs for now'}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = CatImageSerializer(data=request.data)
+def endpoints(request):
+    return render(request, 'endpoints.html')
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class DogImagesLink(generics.ListCreateAPIView):
-    serializer_class = CatImageSerializer
-    def get(self, request):
-        return Response(DogImageSerializer(random.choice(list(DogImage.objects.all())), many=False).data)
-
-    def post(self, request):
-        # don't accept any post requests for now
-        return Response({'error':'not accepting POSTs for now'}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = DogImageSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def documentation(request):
+    return render(request, 'documentation.html')
