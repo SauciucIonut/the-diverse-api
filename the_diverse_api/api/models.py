2f1418b16647d4cf03841a3e6db38c53af8e7e3b
IonutSauciuc <ionut.sauciuc2003@gmail.com>
2021-03-02 20:40:02 +0200

    now every account will have an automatically generated token

--- the_diverse_api/api/models.py
+++ the_diverse_api/api/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+# API MODELS ----------------------------------------
 
 class CatImage(models.Model):
     link = models.CharField(max_length=100)
@@ -13,3 +17,9 @@ class DogImage(models.Model):
 
     def __str__(self):
         return self.link
+
+# TOKEN MODEL ---------------------------------------
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
